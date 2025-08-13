0af548985e19c391a3eb20a678e130af3626910e
Kushal Ner <nerkushal@gmail.com>
2025-08-13 16:26:25 +0530

    Update settings.py

--- attendence/attendence/settings.py
+++ attendence/attendence/settings.py
@@ -81,23 +81,26 @@ WSGI_APPLICATION = 'attendence.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES ={
+#     'default':{
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME' : 'attendence',
+#         'USER' : 'root',
+#         'PASSWORD' : '*Kushal0010#',
+#         'HOST' : 'localhost',
+#         'PORT' : '3306',
 #     }
 # }
-DATABASES ={
-    'default':{
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'attendence',
-        'USER' : 'root',
-        'PASSWORD' : '*Kushal0010#',
-        'HOST' : 'localhost',
-        'PORT' : '3306',
+        'NAME': 'attendence',
+        'USER': 'root',
+        'PASSWORD': '*Kushal0010#',
+        'HOST': 'mydbhost.render.com',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
